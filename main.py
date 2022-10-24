c44e9c83acd67cab28dfd303323087b222caba27
Lynz951 <lindsay.haddix@student.awesomeinc.org>
2022-10-24 16:58:37 +0000

    practice

---
+++ main.py
@@ -0,0 +1,73 @@
+# age = 20
+# price = 19.95
+# first_name = "Lindsay"
+# is_online = True
+# print(age)
+
+# name = input("What is your name? ")
+# print("Hello " + name)
+
+# birth_year = input("Enter your birth year: ")
+# age = 2022 - int(birth_year)
+# print(age)
+
+# First: 10
+# Second: 20
+
+# first = input("First: ")
+# second = input("Second")
+# sum = int(first) + int(second)
+# print("Sum: " + str(sum))
+
+# course = 'Python for Beginners'
+# print(course.find('y')) #1
+# print('Python' in course) #True
+
+# x=10
+# x += 3 
+# print(x)
+
+# price = 25
+# print(price > 10 and price < 30)
+
+# temperature = 5
+
+# if temperature > 30:
+#     print("It's a hot day!")
+#     print("Drink plenty of water")
+# elif temperature > 20:
+#     print("It's a nice day!")
+# elif temperature > 10:
+#     print("It's a bit cold!")
+# else:
+#     print("Its freezing")
+# print("Done")
+
+# Weight: 170
+
+# weight = int(input("Weight: "))
+# unit = input("(K)g or (L)bs: ")
+# if unit.upper == "K":
+#     converted = weight/0.45
+#     print("Weight in Lbs: " + str(converted))1
+# else:
+#     converted = weight * 0.45
+#     print("Weight in Kgs: " + str(converted))
+
+# i = 1
+# while i <= 5:
+#     print(i * "*")
+#     i = i + 1
+
+# names = ["Lindsay", "Amy", "Molly"]
+# print(names[0:2])
+
+numbers = [1, 2, 3, 4, 5]
+for item in numbers:
+    print(item)
+
+i = 0
+while i < len(numbers):
+    print(numbers[i])
+    i = i + 1
+
